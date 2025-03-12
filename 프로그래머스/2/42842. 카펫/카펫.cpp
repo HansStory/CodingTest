5d206b96013237e2264dc00bdbd49c235ff9f30b
Han Stroy <70941747+HansStory@users.noreply.github.com>
2025-03-12 09:51:16 +0900

    [level 2] Title: 카펫, Time: 0.02 ms, Memory: 4.15 MB -BaekjoonHub

---
+++ 프로그래머스/2/42842. 카펫/카펫.cpp
@@ -0,0 +1,24 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+vector<int> solution(int brown, int yellow)
+{
+    vector<int> answer;
+    int total = brown + yellow;
+    
+    for (int h = 3; h <= total / h; h++)
+    {
+        if (total % h == 0)
+        {
+            int w = total / h;
+            if ((w-2) * (h-2) == yellow)
+            {
+                answer = {w, h};
+            }
+        }
+    }
+    
+    return answer;
+}
