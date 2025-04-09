2dc49093c8af832af58e21ed66ee617bb216e77d
Han Stroy <70941747+HansStory@users.noreply.github.com>
2025-04-09 12:08:03 +0900

    [Silver I] Title: 카잉 달력, Time: 24 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/6064. 카잉 달력/카잉 달력.cc
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <numeric>
+
+using namespace std;
+
+int GetYear(int M, int N, int x, int y)
+{
+    //x--; y--;
+    int lastYear = lcm(M, N);
+    int year = x;
+    while (year <= lastYear)
+    {
+        int currentY = (year - 1) % N + 1;
+        
+        if (currentY == y)
+        {
+           return year; 
+        }
+        
+        year += M;
+    }
+    
+    return -1;
+}
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    
+    int T;
+    cin >> T;
+
+    for (int i = 0; i < T; i++)
+    {
+        int M, N, x, y;
+        cin >> M >> N >> x >> y;
+        cout << GetYear(M, N, x, y) << "\n";
+    }
+
+    return 0;
+}
